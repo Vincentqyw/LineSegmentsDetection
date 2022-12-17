fb6698e62cde37d797008cb16b537810463103d3
Realcat <realcat@126.com>
2022-12-17 11:43:43 +0000

    remove submodules

---
+++ run.sh
@@ -0,0 +1,22 @@
+# git rm --cached SOLD2
+# git rm --cached DeepLSD
+# git rm --cached TP-LSD
+# git rm --cached Unified-Line-Segment-Detection
+# git rm --cached LETR
+# git rm --cached Deep-Hough-Transform-Line-Priors
+# git rm --cached F-Clip
+# git rm --cached mlsd
+# git rm --cached afm_cvpr2019
+# git rm --cached lcnn
+# git rm --cached hawp
+rm -rf SOLD2
+rm -rf DeepLSD
+rm -rf TP-LSD
+rm -rf Unified-Line-Segment-Detection
+rm -rf LETR
+rm -rf Deep-Hough-Transform-Line-Priors
+rm -rf F-Clip
+rm -rf mlsd
+rm -rf afm_cvpr2019
+rm -rf lcnn
+rm -rf hawp
